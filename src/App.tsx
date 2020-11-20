88aeb1bad567f0a189ecd2cdad92fb67766383c1
Natan Cardoso <natancb7@gmail.com>
2020-11-20 19:30:32 -0300

    ADD - Layout Landing Page e images svg

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,34 @@
 import React from 'react';
+import { FiArrowRight } from 'react-icons/fi';
 
-function App() {
-    return (
-        <div className="App">
-            <h1>Hello World</h1>
-        </div>
-    );
+import './styles/global.scss';
+
+import logoImg from './images/logo.svg';
+
+interface Props {
+    name?: string;
 }
+
+const App: React.FC<Props> = (props) => (
+    <div id="page-landing">
+        <div className="content-wrapper">
+            <img src={logoImg} alt="Happy" />
+
+            <main>
+                <h1>Leve felicidade para o mundo</h1>
+                <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+            </main>
+
+            <div className="location">
+                <strong>João Pessoa</strong>
+                <span>Paraíba</span>
+            </div>
+
+            <a href="#" className="enter-app">
+                <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)" />
+            </a>
+        </div>
+    </div>
+);
 
 export default App;
